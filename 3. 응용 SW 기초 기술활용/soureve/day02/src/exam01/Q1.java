e1a073077bd31b9176aad23c62fa070fa528a2ba
jeyoungyu <dbwpdud85@gmail.com>
2024-04-15 16:10:11 +0900

    연산자 / 조건문 / 반복문 / 배열

---
+++ 3. 응용 SW 기초 기술활용/soureve/day02/src/exam01/Q1.java
@@ -0,0 +1,20 @@
+package exam01;
+
+public class Q1 {
+    public static void main(String[] args) {
+       /* 변수 두 개를 선언해서 20과 3.0을 대입하고 두 변수의 사칙연산 결과를 정수로 출력해 보시오
+       int a = 20;
+        double b = 3.0;
+        int c = a + (int)b;
+
+        System.out.println(c); */
+
+        int num1 = 20;
+        double num2 = 3.9;
+
+        int result = (int)(num1 * num2);
+        System.out.println(result);
+
+
+    }
+}
---
+++ 3. 응용 SW 기초 기술활용/soureve/day02/src/exam01/Q4.java
@@ -0,0 +1,27 @@
+package exam01;
+
+public class Q4 {
+    public static void main(String[] args) {
+        /* 구구단을 홀수 단만 출력하도록 프로그램을 만드시오. 구현 소스
+        for(int i = 2; i <=9; i++) {
+            System.out.println(i+ "단입니다ㅡㅡㅡㅡㅡㅡㅡ");
+
+            for(int j = 1; j <= 9 ; j++) {
+                int k = i/2;
+                System.out.printf("%d X %d = %d\n" , i , k ,(i*k));
+
+            }
+        } */
+
+        for ( int i = 2; i <= 9; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+                System.out.println(i + "단입니다ㅡㅡㅡㅡㅡㅡㅡ");
+                for (int j = 1; j <= 9; j++) {
+                    System.out.printf("%d X %d = %d\n", i, j, i * j);
+                }
+            }
+
+    }
+}
